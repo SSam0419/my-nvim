1eea55845edef82f87756df46501a7dcf3bfabae
SSam0419 <leemingchun419@gmail.com>
2025-02-21 02:17:21 +0000

    fix lsp

--- init.lua
+++ init.lua
@@ -249,7 +249,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -259,7 +259,7 @@ require('lazy').setup({
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
-      -- vim.cmd 'colorscheme rose-pine'
+      vim.cmd 'colorscheme rose-pine'
     end,
   },
   -- Highlight todo, notes, etc in comments
